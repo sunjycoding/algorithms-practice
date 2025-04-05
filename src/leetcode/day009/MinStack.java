28c58f98b1658e1d17b4bdac7110913106357ff8
sunjy <sunjyleisure@gmail.com>
2025-04-05 09:27:14 -0700

    feat: day009 of leetcode

---
+++ src/leetcode/day009/MinStack.java
@@ -0,0 +1,45 @@
+package leetcode.day009;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 155. Min Stack
+ *
+ * @author Created by sunjy on 4/4/25
+ */
+public class MinStack {
+
+    private final Deque<Integer> stack;
+    private final Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            stack.pop();
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
